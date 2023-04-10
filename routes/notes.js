14e1b5a18b229322be6cc9730b2f8f0f5e71d653
darylbg <darylxcuf@gmail.com>
2023-04-10 14:02:50 +0100

    added post request and note saving

---
+++ routes/notes.js
@@ -0,0 +1,60 @@
+const notesRouter = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+// GET route retrives all data from db.json
+notesRouter.get('/', (req, res) => {
+    fs.promises.readFile('./db/db.json')
+        .then(data => res.json(JSON.parse(data)))
+        .catch(error => {
+        console.error(error);
+        return res.status(500).json({ message: 'Error reading data from file' });
+        });
+});
+
+notesRouter.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const note = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        fs.readFile('./db/db.json', (error, data) => {
+            if (error) {
+                throw error
+            } else {
+                const notes = JSON.parse(data);
+                notes.push(note);
+
+                fs.writeFile('./db/db.json', JSON.stringify(notes), (error) => {
+                    if (error) {
+                        throw error;
+                    } else {
+                        console.log('successfully saved note to db.json');
+                    }
+                });
+            }
+        })
+
+        const response = {
+            status: 'success',
+            body: note
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in creating note')
+    }
+
+});
+
+notesRouter.delete('/:notes_id', (req, res) => {
+    const { note_id } = req.params;
+
+    res.send(`note id: ${note_id} deleted`)
+});
+
+module.exports = notesRouter;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/api', api);
 
 // GET route for home page
 app.get('/', (req, res) => 
